4eebef46f3a83ace78f0481da7fe3e9fb71936dd
vagabond911 <vageeshbits@gmail.com>
2026-01-04 23:36:12 -0500

    sdk changed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,7 +26,7 @@ android {
     defaultConfig {
         applicationId = "com.example.tiptime"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
